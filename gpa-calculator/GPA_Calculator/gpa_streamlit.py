b44d117154fa2a43042bcc9379ce0be3bd161510
Arpeet Shah <asyour1friend@gmail.com>
2026-01-09 23:22:45 -0600

    Updated app: persistent MS grades, gradient, bubble tabs, analytics

--- gpa-calculator/GPA_Calculator/gpa_streamlit.py
+++ gpa-calculator/GPA_Calculator/gpa_streamlit.py
@@ -380,13 +380,13 @@ with main_tabs[0]:
         )
 
     # ---------- COLUMN 3: Image ----------
+
+    # ----- RIGHT: Welcome image -----
     with col_image:
         st.image(
             "https://images.unsplash.com/photo-1589629828693-5533d7a9d731?auto=format&fit=crop&w=900&q=80",
-            width=380,
+            width=500,
         )
-
-    # ----- RIGHT: Welcome image ----
         # =============================
 # TAB 3: DAILY DASHBOARD
 with main_tabs[3]:
